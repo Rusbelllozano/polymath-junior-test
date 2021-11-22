1409a43e091f4b210ee556201fac82de8ca34fe9
rusbelllozano <ruslllozano@gmail.com>
2021-11-22 17:54:51 -0500

    fix error in viewCharacter

--- src/components/BoxCharacter/BoxCharacter.js
+++ src/components/BoxCharacter/BoxCharacter.js
@@ -6,7 +6,6 @@ export default {
   methods: {
     viewCharacter() {
       this.$store.commit("view_character", this.character_info);
-      this.$store.commit("check_view_character", this.character_info);
     },
     checkViewed() {
       this.$store.commit("view_character", null);
@@ -14,6 +13,7 @@ export default {
     selectCharacter() {
       this.$store.commit("view_character", this.character_info);
       this.$store.commit("showCharacterInfo");
+      this.$store.commit("check_view_character", this.character_info);
     },
   },
 };
